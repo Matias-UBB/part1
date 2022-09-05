d7205a8393e42c739bfd9a8ee3b24a21a7bdc9ad
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2022-09-05 15:59:07 -0400

    Se inicia el proyecto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="george"/>
+      <Hello name="pedro"/>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root')) 
+
