69e447001022fc92b46f92d7afc5dfd44331222a
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2022-09-13 20:11:13 -0400

    Arrai de objetos y props a componentes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
-    <div>
-      <Header course={course}/>
-      <Content part={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
-      <Total Total= {exercises1 + exercises2 + exercises3} />
+   <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -29,10 +37,11 @@ const Header = (props) => {
 const Content=(props)=>{
   console.log(props)
   return (
+
     <div>
-      <Part part={props.part[0]} exercises={props.exercises[0]} />
-      <Part part={props.part[1]} exercises={props.exercises[1]} />
-      <Part part={props.part[2]} exercises={props.exercises[2]} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>   
   )
 } 
@@ -45,7 +54,7 @@ return(
 const Total=(props)=>{
   return (
     <div>
-      <p>Number of exercises {props.Total} </p>
+      <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises} </p>
     </div>
   )
 } 
